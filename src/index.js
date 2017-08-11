e17ddb7a463ad80c0315f97a65579bbb428b0434
Matt Styles <matt@personalurban.com>
2017-08-11 21:08:49 +0100

    initial commit :tada:

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import {render} from 'react-dom'
+import {Signal} from 'raid'
+
+import Title from './component'
+
+const signal = new Signal({})
+
+signal.observe(state => {
+  render(
+    <Title text='yo dudes' />,
+    document.querySelector('.js-main')
+  )
+}, err => console.error(err))
